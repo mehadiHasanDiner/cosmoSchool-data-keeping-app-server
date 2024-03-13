c5120f9ac9ad6050002635a2b1582dc491a7dce4
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-03-14 02:54:39 +0600

    apin done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ehabgxd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -46,6 +46,20 @@ async function run() {
         .find(query)
         .sort({ createdAt: -1 })
         .toArray();
+      res.send(result);
+    });
+
+    // load specific item (but not taking all of the specific service)
+    app.get("/addpurchase/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const options = {
+        // Include only the `title` and `imdb` fields in each returned document
+        projection: { itemName: 1, itemCategory: 1, branchName: 1 },
+      };
+
+      const result = await itemsCollection.findOne(query, options);
       res.send(result);
     });
 
